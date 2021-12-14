c106b921329fcca2a1a060037bca041db8070d6d
성현Maria 김 <globalkmaria@gmail.com>
2021-12-15 08:18:37 +0900

    Add : login -> 로그인 rest api 인가, 토큰 받기

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+
+const REST_API_KEY = '41a42b07d20ba6ef41277371b9f0e93b';
+const REDIRECT_URI = 'http://localhost:3000/login';
+const URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&prompt=login`;
 
 function Login() {
-  return <div />;
+  const navigate = useNavigate();
+  const [search, setSearch] = useSearchParams();
+  const AUTHORIZE_CODE = search.get('code');
+
+  if (!!AUTHORIZE_CODE) {
+    fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({
+        grant_type: 'authorization_code',
+        client_id: REST_API_KEY,
+        redirect_uri: REDIRECT_URI,
+        code: AUTHORIZE_CODE,
+      }),
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      });
+  }
+  console.log('AUTHORIZE_CODE', AUTHORIZE_CODE);
+  return (
+    <div>
+      <a href={URL}>카카오 로그인</a>
+      <Link to="/">홈</Link>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Main() {
-  return <div />;
+  return (
+    <div>
+      <Link to="/login">로그인</Link>
+      <button>로그아웃</button>
+    </div>
+  );
 }
 
 export default Main;
