a5d39b6d24bc072069becd23d9c216f4b9bb9a8e
성현Maria 김 <globalkmaria@gmail.com>
2021-12-15 08:28:50 +0900

    console 제거

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,9 +6,9 @@ const REDIRECT_URI = 'http://localhost:3000/login';
 const URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&prompt=login`;
 
 function Login() {
-  const navigate = useNavigate();
-  const [search, setSearch] = useSearchParams();
-  const AUTHORIZE_CODE = search.get('code');
+  // const navigate = useNavigate();
+  // const [search, setSearch] = useSearchParams();
+  // const AUTHORIZE_CODE = search.get('code');
   // 백엔드에 AUTHORIZE_CODE 전달
   // if (!!AUTHORIZE_CODE) {
   //   fetch('백엔드URL', {
@@ -24,7 +24,7 @@ function Login() {
   //       alert('로그인 되었습니다!');
   //     });
   // }
-  console.log('AUTHORIZE_CODE', AUTHORIZE_CODE);
+  // console.log('AUTHORIZE_CODE', AUTHORIZE_CODE);
   return (
     <div>
       <a href={URL}>카카오 로그인</a>
