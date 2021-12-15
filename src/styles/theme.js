647a39becab1d51e4f5fda0e668b850b98a4220c
howyoujini <howyoujini@gmail.com>
2021-12-15 13:38:20 +0900

    Add: Nav Bar
    Modified: Review 반영

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,14 +1,14 @@
 const theme = {
-  pointPink: '#BF565F',
-  pointNavy: '#364259',
-  pointBeige: ' #F2C0A2',
+  pointYellow: '#EAF207',
+  pointCobalt: '#643DF2',
+  pointDarkCobalt: '#5D3BD9',
   veryDarkGrey: '#585858',
   darkGrey: '#7F7F7F',
   grey: '#A8A8A8',
   lightGrey: '#CCCCCC',
   veryLightGrey: '#E0E0E0',
-  black: '#141414',
-  white: '#FFFFFF',
+  black: '#0D0D0D',
+  white: '#F2F2F2',
 };
 
 export default theme;
