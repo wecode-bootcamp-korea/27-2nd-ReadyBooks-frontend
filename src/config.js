ed0742b71c4d3877c5e1a671db0b7dbef5ca1c12
성현Maria 김 <globalkmaria@gmail.com>
2021-12-16 22:13:29 +0900

    Modify: login 백엔드 통신 endpoint 변경

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ const BASELINE_JONGHO = 'http://10.58.6.17:8000';
 
 export const API = {
   cart: `${BASELINE}/cart`,
-  login: `${BASELINE_JONGHO}/users`,
+  login: `${BASELINE_JONGHO}/users/kakao/signin`,
 };
