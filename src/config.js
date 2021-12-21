aecce39c0c640d45a7873b5bd1c1081cbfb2acb9
성현Maria 김 <globalkmaria@gmail.com>
2021-12-21 21:28:14 +0900

    Modify : 백엔드 get 부분 변경확인

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
-const BASELINE_JONGHO = 'http://10.58.6.17:8000';
+// const BASELINE_JONGHO = 'http://10.58.6.17:8000';
 const BASELINE_JUHO = 'http://10.58.7.3:8000';
-const BASELINE_JINSUNG = 'http://10.58.0.42:8000';
+// const BASELINE_JUNGHO = 'http://10.58.0.105:8000';
+const BASELINE_JINSUNG = 'http://10.58.5.23:8000';
 
 export const API = {
-  carts: `${BASELINE_JUHO}/carts`,
-  login: `${BASELINE_JUHO}/users/kakao/signin`,
+  carts: `${BASELINE_JINSUNG}/carts`,
+  login: `${BASELINE_JINSUNG}/users/kakao/signin`,
   book: `${BASELINE_JINSUNG}/books`,
   review: `${BASELINE_JINSUNG}/books/review`,
-  books: `${BASELINE_JUHO}/books/all`,
+  books: `${BASELINE_JINSUNG}/books/all`,
+  orders: `${BASELINE_JINSUNG}/orders`,
 };
