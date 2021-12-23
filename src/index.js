117bfca1dc90f30a261dae4f96db06396b680b63
성현Maria 김 <globalkmaria@gmail.com>
2021-12-23 12:31:38 +0900

    Add : pdf popup 할 때 scroll 없애기

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
-// import PdfViewerComponent from './components/PdfViewerComponent';
 import theme from './styles/theme.js';
 import { pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -13,7 +12,6 @@ ReactDOM.render(
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <Router />
-      {/* <PdfViewerComponent document="Document.pdf" /> */}
     </ThemeProvider>
   </>,
   document.getElementById('root')
